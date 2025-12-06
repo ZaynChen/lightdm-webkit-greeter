429bad8bcca943d716d188a16043c10a018de116
Zayn Chen <zaynchen@qq.com>
2025-12-06 16:33:28 +0800

    chore(license): using spdx spec

--- greeter/src/bridge/mod.rs
+++ greeter/src/bridge/mod.rs
@@ -1,21 +1,6 @@
-// mod.rs
+// SPDX-FileCopyrightText: 2025 ZaynChen
 //
-// Copyright (C) 2025 ZaynChen
-//
-// This file is part of LightDM WebKit Greeter
-//
-// LightDM WebKit Greeter is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// LightDM WebKit Greeter is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 mod greeter_comm;
 mod greeter_config;
